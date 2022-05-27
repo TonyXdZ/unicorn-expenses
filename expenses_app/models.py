9946f701699e84fee7eefecc3f3ffd3809d4f7ec
TonyXdZ <tony23dz@gmail.com>
2022-05-27 21:24:31 +0100

    Create Expense model

--- expenses_app/models.py
+++ expenses_app/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Expense(models.Model): 
+    short_name = models.CharField(max_length=255)
+    description = models.TextField()
+    amount_spent = models.PositiveIntegerField()
+    date_spent = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'Expense {self.short_name}'
